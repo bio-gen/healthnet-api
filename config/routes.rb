8bc1df10290d2d46a4e91f3defe74109ccfad094
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-12 13:07:57 +0000

    Add API subdomain, module and version to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      resources :users, except: [:new, :edit]
+    end
+  end
 end
