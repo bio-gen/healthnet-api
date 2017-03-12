09a9b69a5feec492252c6866d861780b3f2e12a6
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-12 11:07:19 +0000

    Add active model serializers config

---
+++ config/initializers/ams.rb
@@ -0,0 +1,9 @@
+# ActiveModelSerializers with JSON API Specification
+ActiveModelSerializers.config.adapter = :json_api
+
+ActiveModelSerializers.config.key_transform = :unaltered
+
+# In order to properly handle JSON API responses, we need to register a JSON API renderer, like so:
+ActiveSupport.on_load(:action_controller) do
+  require 'active_model_serializers/register_jsonapi_renderer'
+end
