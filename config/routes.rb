1112670a7b9a0ff463e4685d0d70f72ce441d1b4
Jaime Alvarez <jaime_andres@unitec.edu>
2017-03-13 07:20:38 +0000

    Remove API subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'api/v1/users/registrations' }
 
-  namespace :api, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+  namespace :api, path: '' do
+    namespace :v1, path: '' do
       post 'auth_user', to: 'authentication#authenticate_user'
 
       resources :users, except: [:new, :edit]
