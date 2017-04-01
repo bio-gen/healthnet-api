3a215f4139b1314b2d712f6026f411659061db15
Jaime Alvarez <jaime_andres@unitec.edu>
2017-04-01 07:11:46 +0000

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password '12345678'
     password_confirmation '12345678'
+    professional_experience { Faker::Lorem.paragraph(5) }
+    educational_background { Faker::Lorem.paragraph(5) }
   end
 end
