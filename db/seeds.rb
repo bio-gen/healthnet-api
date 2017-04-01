f71f7171aa5edda9616901bf07fd6a4d718c5fb8
Jaime Alvarez <jaime_andres@unitec.edu>
2017-04-01 07:11:32 +0000

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 # Create a test user
-User.create(email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
+User.create(email: 'test@test.com',
+    password: '12345678',
+    password_confirmation: '12345678',
+    professional_experience: Faker::Lorem.paragraph(5),
+    educational_background: Faker::Lorem.paragraph(5)
+    )
