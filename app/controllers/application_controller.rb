490d9cb74e56d8f243117d5df29db6b352ef98ee
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-01 08:22:03 +0000

    Add knock to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,7 @@
 class ApplicationController < ActionController::API
+  include Knock::Authenticable
 
-  attr_reader :current_user
+  before_action :authenticate
 
-  protected
-
-    # This method will act as a before_filter to check user credentials.
-    # The AuthenticationController will handle all authentication requests to the API
-    def authenticate_request!
-      unless user_id_in_token?
-        render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-        return
-      end
-      @current_user = User.find(auth_token[:user_id])
-    rescue JWT::VerificationError, JWT::DecodeError
-      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-    end
-
-  private
-
-    def http_token
-      @http_token ||= if request.headers['Authorization'].present?
-        request.headers['Authorization'].split(' ').last
-      end
-    end
-
-    def auth_token
-      @auth_toke ||= JsonWebToken.decode(http_token)
-    end
-
-    def user_id_in_token?
-      http_token && auth_token && auth_token[:user_id].to_i
-    end
 
 end
