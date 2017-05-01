307d2dfa834ae2b702a155615300ea0630b304ab
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-01 08:43:45 +0000

    edit knock route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { registrations: 'api/v1/users/registrations' }
 
   scope module: 'api' do
     namespace :v1 do
       resources :users, except: [:new, :edit]
 
-      mount Knock::Engine => "/knock"
+      post 'user_token' => 'user_token#create'
     end
   end
 end
