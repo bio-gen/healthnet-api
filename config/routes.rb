237440c6a26ecee84ea7d55b40df90799a4a0238
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-01 08:58:41 +0000

    modify knock auth route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: [:new, :edit]
 
-      post 'user_token' => 'user_token#create'
+      post 'auth' => 'user_token#create'
     end
   end
 end
---
+++ db/migrate/20170501084556_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+
+      t.string :full_name
+      t.text :professional_experience
+      t.text :educational_background
+      t.string :photo
+      t.string :user_type
+
+      t.timestamps
+    end
+  end
+end
