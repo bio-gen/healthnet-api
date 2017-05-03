551d98547d3f9eca3c24ab340b0a43e726e47676
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-03 01:41:57 +0000

    Add user e-mail uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
 
   context "validations" do
     it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of :email }
     it { is_expected.to validate_confirmation_of :password }
   end
 end
