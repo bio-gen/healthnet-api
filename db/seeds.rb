71ae467a83d4c31c4772acd75a7d33884244434d
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-08 03:28:40 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 # Create a test user
-User.create(email: 'test@test.com',
-    password: '12345678',
-    password_confirmation: '12345678',
-    full_name: 'Doctor Mengueche'
-    )
+user = User.create(
+  email: 'test@test.com',
+  password: '12345678',
+  password_confirmation: '12345678',
+  full_name: 'Doctor Mengueche'
+  )
+
+user.work_experiences.create(
+  title: 'Software Engineer',
+  company: 'Google',
+  location: 'USA',
+  start_date: Date.new,
+  current: true,
+  description: 'Lalalala'
+)
