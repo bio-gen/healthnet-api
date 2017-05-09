ce3f894d3033ea7312a80587f3103ef82933f979
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-09 08:08:26 +0000

    Add JSON response request spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 require 'support/shoulda'
 require 'support/database_cleaner'
+require 'support/request_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -58,4 +59,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Request::JsonHelpers, type: :request
 end
