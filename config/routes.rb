b939afad98b1f5874cbf17f9d87086235018cc70
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-15 06:58:35 +0000

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
-      resources :users, except: [:new, :edit] do
-        resources :work_experiences, except: [:new, :edit]
-        resources :educations, except: [:new, :edit]
+      resources :users do
+        resources :work_experiences
+        resources :educations
       end
 
       post 'auth' => 'user_token#create'
--- spec/routing/authentication_routing_spec.rb
+++ spec/routing/authentication_routing_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AuthenticationController, type: :routing do
-  describe 'routing' do
-    it 'routes to /v1/auth' do
+  describe 'authentication routing' do
+    it 'routes to /v1/auth to user_token#create' do
       expect(:post => '/v1/auth').to route_to('api/v1/user_token#create')
     end
   end
