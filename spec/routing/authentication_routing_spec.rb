f585a9785e1f11323f0ad69933ffe036aaf92b62
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-15 09:44:50 +0000

    remove old auth controller and fix routing spec

--- spec/routing/authentication_routing_spec.rb
+++ spec/routing/authentication_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::AuthenticationController, type: :routing do
+RSpec.describe Api::V1::UserTokenController, type: :routing do
   describe 'authentication routing' do
     it 'routes to /v1/auth to user_token#create' do
       expect(:post => '/v1/auth').to route_to('api/v1/user_token#create')
