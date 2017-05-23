9daa91174c7aaeda13927cbdc6fddf71311c0d27
Jaime Alvarez <jaime_andres@unitec.edu>
2017-05-23 01:59:52 +0000

    serialize extra fields

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name
+  attributes :id, :email, :first_name, :last_name, :title, :location, :institution, :date_of_birth, :phone_number
 
   has_many :work_experiences
   has_many :educations
